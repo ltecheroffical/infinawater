137e3248fbf2f192a04f8f17aa4b6a61e9bdef09
LTecher Offical <ltecheroffical@gmail.com>
2025-09-24 20:32:28 -0400

    Flowing down water turns into source blocks

---
+++ src/main/java/net/ltecher/infinawater/mixin/MixinFlowableFluid.java
@@ -0,0 +1,86 @@
+package net.ltecher.infinawater.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.ltecher.infinawater.InfinaWater;
+import net.minecraft.block.BlockState;
+import net.minecraft.fluid.FlowableFluid;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+
+@Mixin(FlowableFluid.class)
+public abstract class MixinFlowableFluid {
+    /*@Inject(method = "getSpread", at = @At("HEAD"), cancellable = true)
+    private void onGetSpread(ServerWorld world, BlockPos pos, BlockState state, CallbackInfoReturnable<Map<Direction, FluidState>> cir) {
+		boolean infiniteLiquids = world.getGameRules().getBoolean(InfinaWater.GAMERULE_DO_INFINITE_LIQUIDS);
+		if (!infiniteLiquids) {
+			return;
+		}
+
+		HashMap<Direction, FluidState> spread = new HashMap<Direction, FluidState>();
+		for (var direction : Direction.values()) {
+			if (direction == Direction.UP) {
+				continue;
+			}
+
+			BlockPos neighborPos = pos.offset(direction);
+			BlockState neighborState = world.getBlockState(neighborPos);
+
+			if (!neighborState.contains(Properties.WATERLOGGED)) {
+				if (!neighborState.canBucketPlace(neighborState.getFluidState().getFluid())) {
+					continue;
+				}
+
+				if (!neighborState.getFluidState().isEmpty()) {
+					continue;
+				}
+			}
+
+            FlowableFluid self = (FlowableFluid) (Object) this;
+			spread.put(direction, self.getStill(false));
+		}
+		
+		cir.setReturnValue(spread);
+    }*/
+
+
+    @Inject(method = "tryFlow", at = @At("HEAD"), cancellable = true)
+	private void onTryFlow(ServerWorld world, BlockPos fluidPos, BlockState blockState, FluidState fluidState, CallbackInfo ci) {
+		boolean infiniteLiquids = world.getGameRules().getBoolean(InfinaWater.GAMERULE_DO_INFINITE_LIQUIDS);
+		if (!infiniteLiquids) {
+			return;
+		}
+
+		ci.cancel();
+		FlowableFluid self = (FlowableFluid) (Object) this;
+		for (var direction : Direction.values()) {
+			if (direction == Direction.UP) {
+				continue;
+			}
+
+			BlockPos neighborPos = fluidPos.offset(direction);
+			BlockState neighborState = world.getBlockState(neighborPos);
+
+			if (!neighborState.contains(Properties.WATERLOGGED)) {
+				if (!neighborState.canBucketPlace(neighborState.getFluidState().getFluid())) {
+					continue;
+				}
+
+				if (!neighborState.getFluidState().isEmpty()) {
+					continue;
+				}
+			} else {
+				world.setBlockState(neighborPos, neighborState.with(Properties.WATERLOGGED, true), 3);
+				continue;
+			}
+
+			world.setBlockState(neighborPos, self.getStill(true).getBlockState());
+		}
+	}
+}
